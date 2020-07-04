260cd1bc6eea74b6ce426e85b2174f0bd401c989
Daniel Gonçalves <danielgg0831@gmail.com>
2020-07-04 15:50:10 -0300

    refactor: remove duplicate check for required fields

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,12 +4,13 @@ import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   public handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    } else if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
-    } else {
-      return {} as any
+    const requiredFields = ['name', 'email']
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
+    return {} as any
   }
 }
