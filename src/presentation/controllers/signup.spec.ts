eb4fd47aa820933c455db5bef16a1133953536a4
Daniel Gonçalves <danielgg0831@gmail.com>
2020-07-04 18:21:38 -0300

    refactor: move all error import to a single file

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,9 +1,7 @@
 import { SignUpController } from './signup'
-import { MissingParamError } from '../errors/missing-param-error'
 import { EmailValidator } from '../protocols/email-validator'
 import { mock, MockProxy } from 'jest-mock-extended'
-import { InvalidParamError } from '../errors/invalid-param-error'
-import { ServerError } from '../errors/server-error'
+import { MissingParamError, InvalidParamError, ServerError } from '../errors'
 
 interface SutType {
   sut: SignUpController
