013c56c2e6b4ab3de66210e8cfe9f8de0d4e31a4
Daniel Gonçalves <danielgg0831@gmail.com>
2020-07-04 19:14:57 -0300

    test: ensure SignUpController returns 200 if valid values are provided

--- src/presentation/controllers/sign-up/signup.spec.ts
+++ src/presentation/controllers/sign-up/signup.spec.ts
@@ -15,10 +15,10 @@ const makeSut = (): SutType => {
 
   const addAccountStub = mock<AddAccount>()
   addAccountStub.add.mockReturnValue({
-    id: 'any_id',
-    name: 'any_name',
-    email: 'any_email@email.com',
-    password: 'any_password'
+    id: 'valid_id',
+    name: 'valid_name',
+    email: 'valid_email@email.com',
+    password: 'valid_password'
   })
 
   const sut = new SignUpController(emailValidatorStub, addAccountStub)
@@ -198,6 +198,28 @@ describe('SignUp Controller', () => {
       const httpResponse = sut.handle(httpRequest)
       expect(httpResponse.statusCode).toBe(500)
       expect(httpResponse.body).toEqual(new ServerError())
+    })
+
+    test('Should return 200 if if valid data is provided', () => {
+      const { sut } = makeSut()
+
+      const httpRequest = {
+        body: {
+          name: 'valid_name',
+          email: 'valid_email@email.com',
+          password: 'valid_password',
+          passwordConfirmation: 'valid_password'
+        }
+      }
+
+      const httpResponse = sut.handle(httpRequest)
+      expect(httpResponse.statusCode).toBe(200)
+      expect(httpResponse.body).toEqual({
+        id: 'valid_id',
+        name: 'valid_name',
+        email: 'valid_email@email.com',
+        password: 'valid_password'
+      })
     })
   })
 })
