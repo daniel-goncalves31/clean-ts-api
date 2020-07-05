2aba225f7a043d9af99feb1080167f4c0ae034d2
Daniel Gonçalves <danielgg0831@gmail.com>
2020-07-05 19:41:57 -0300

    test: ensure LoginController returns 400 if no email is provided

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,31 @@
+import { LoginController } from './login'
+import { HttpRequest } from '../sign-up/signup-protocols'
+import { badRequest } from '../../helpers/http-helpers'
+import { MissingParamError } from '../../errors'
+
+interface SutType {
+  sut: LoginController
+}
+
+const makeSut = (): SutType => {
+  const sut = new LoginController()
+
+  return {
+    sut
+  }
+}
+
+describe('LoginController', () => {
+  test('should return 400 if no email is provided', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest: HttpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+})
