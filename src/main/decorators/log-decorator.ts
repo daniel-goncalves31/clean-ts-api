a2c1c9437cff38c2fcd488fa89c9c25e99691cf9
Daniel Gonçalves <danielgg0831@gmail.com>
2020-07-05 19:35:09 -0300

    refactor: remove console.log

--- src/main/decorators/log-decorator.ts
+++ src/main/decorators/log-decorator.ts
@@ -9,7 +9,6 @@ export class LogControllerDecorator implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const response = await this.controller.handle(httpRequest)
-    console.log(response.body.stack)
     if (response.statusCode === 500) {
       await this.logErrorRespository.logError(response.body.stack)
     }
