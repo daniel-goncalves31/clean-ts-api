5f2ce40c5b98c95feb1452946caa103d4bc796de
Daniel Gonçalves <danielgg0831@gmail.com>
2020-07-05 19:29:50 -0300

    refactor: inject LogMongoRepository in the LogControllerDecorator class

--- src/main/factories/sign-up.ts
+++ src/main/factories/sign-up.ts
@@ -3,8 +3,9 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
-import { Controller } from '@/presentation/protocols'
+import { Controller } from '../../presentation/protocols'
 import { LogControllerDecorator } from '../decorators/log-decorator'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-mongo/log-mongo-repository'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
@@ -16,5 +17,6 @@ export const makeSignUpController = (): Controller => {
     emailValidatorAdapter,
     dbAddAccount
   )
-  return new LogControllerDecorator(signUpController)
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signUpController, logMongoRepository)
 }
