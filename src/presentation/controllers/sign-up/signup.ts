c7f26b55571b63ad8ad09346da8386a1caf5e0ef
Daniel Gonçalves <danielgg0831@gmail.com>
2020-07-06 18:17:48 -0300

    refactor: remove fields validation from the SignUpController

--- src/presentation/controllers/sign-up/signup.ts
+++ src/presentation/controllers/sign-up/signup.ts
@@ -1,5 +1,5 @@
 import { badRequest, serverError, ok } from '../../helpers/http-helpers'
-import { MissingParamError, InvalidParamError } from '../../errors'
+import { InvalidParamError } from '../../errors'
 import {
   AddAccount,
   EmailValidator,
@@ -21,19 +21,6 @@ export class SignUpController implements Controller {
       const error = this.validation.validate(httpRequest.body)
       if (error) {
         return badRequest(error)
-      }
-
-      const requiredFields = [
-        'name',
-        'email',
-        'password',
-        'passwordConfirmation'
-      ]
-
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
       }
 
       const { name, email, passwordConfirmation, password } = httpRequest.body
