8c6c4e68af18b4fa6568868da83f76e78119ecfe
Daniel Gonçalves <danielgg0831@gmail.com>
2020-07-06 18:14:52 -0300

    test: ensure SignUpController return 400 if validation fails

--- src/presentation/controllers/sign-up/signup.spec.ts
+++ src/presentation/controllers/sign-up/signup.spec.ts
@@ -221,5 +221,17 @@ describe('SignUp Controller', () => {
       await sut.handle(makeFakeRequest())
       expect(validationStub.validate).toBeCalledWith(makeFakeRequest().body)
     })
+
+    test('should return 400 if Validation fails', async () => {
+      const { sut, validationStub } = makeSut()
+      validationStub.validate.mockReturnValueOnce(
+        new MissingParamError('any_field')
+      )
+
+      const httpResponse = await sut.handle(makeFakeRequest())
+      expect(httpResponse).toEqual(
+        badRequest(new MissingParamError('any_field'))
+      )
+    })
   })
 })
